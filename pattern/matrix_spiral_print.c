7506f6affd8ff6746cdc4d02c6ed79b7443d9f0a
jayapalreddy540 <jayapalreddymjpal@gmail.com>
2019-06-11 11:28:47 +0530

    code update for spiral matrix printing

--- pattern/matrix_spiral_print.c
+++ pattern/matrix_spiral_print.c
@@ -1,24 +1,33 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-	int r=4,c=4;
-	int A[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-	int m=0,n=0,i;
+    int t;
+    scanf("%d",&t);
+    int i,j;
+    int A[t][t];
+    for(i=0;i<t;i++)
+    {
+        for(j=0;j<t;j++)
+            scanf("%d",&A[i][j]);
+    }
+    int r=t,c=t;
+    int m=0,n=0;
          
-	while(m<r&&n<c)
-	{
-		for(i=n;i<c;i++)
-			printf("%d ",A[m][i]);
-		m++;
-		for(i=m;i<r;i++)
-			printf("%d ",A[i][c-1]);
-		c--;
-		for(i=c-1;i>n;i--)
-			printf("%d ",A[r-1][i]);
-		r--;
-		for(i=r;i>=m;i--)
-			printf("%d ",A[i][n]);
-		n++;
-	}
+    while(m<r&&n<c)
+    {
+        for(i=n;i<c;i++)
+            printf("%d ",A[m][i]);
+        m++;
+        for(i=m;i<r;i++)
+            printf("%d ",A[i][c-1]);
+        c--;
+        for(i=c-1;i>n;i--)
+            printf("%d ",A[r-1][i]);
+        r--;
+        for(i=r;i>=m;i--)
+            printf("%d ",A[i][n]);
+        n++;
+    }
     return 0;
 }
